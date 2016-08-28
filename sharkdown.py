7ac15059f87fb778a058b106904f385e98b3ea60
ronrihoo <ron.rihoo@gmail.com>
2016-08-28 00:22:23 -0500

    Update

--- sharkdown.py
+++ sharkdown.py
@@ -11,6 +11,9 @@ class MainWindow(QMainWindow, sharkdown_ui.Ui_Sharkdown):
         QMainWindow.__init__(self)
         QMainWindow.setWindowTitle(self, AppConstants.data['properties']['title'])
         self.setupUi(self)
+        self.filepath = "-1"
+        self.filename = "-1"
+        self.fullpath = ""
         # MENU - FILE
         self.actionNew.setText(AppConstants.data['actions']['new'])
         self.actionLoad.setText(AppConstants.data['actions']['load'])
@@ -49,32 +52,50 @@ class MainWindow(QMainWindow, sharkdown_ui.Ui_Sharkdown):
 
     def new_file(self):
         self.Markdown.setText('')
+        self.filename = "-1"
+        self.fullpath = ""
+        # do not reset filepath; it's used for staying in the last given directory
+
+    def decide_path(self):
+        self.filepath = os.getcwd() if self.filepath == "-1" else self.filepath
+
+    def learn_path(self, fullpath):
+        self.filepath, self.filename = os.path.split(fullpath)
 
     def load_file(self):
         filedialog = QFileDialog()
+        self.decide_path()
         try:
-            filename = QFileDialog.getOpenFileName(filedialog, 'Open File', os.path.expanduser('~'),
-                                                   "Markdown (*.md, *.markdown); Text (*.txt)")
-            with open(filename, 'r') as f:
+            self.fullpath = QFileDialog.getOpenFileName(filedialog, 'Open File', self.filepath,
+                                                        "Markdown (*.md, *.markdown); Text (*.txt)")
+            with open(self.fullpath, 'r') as f:
+                self.learn_path(self.fullpath)
                 self.Markdown.setText('')
                 self.Markdown.insertPlainText(f.read())
         except:
-            print("'Load' operation closed.")
+            print("'Load' operation stopped.")
             pass
 
     def save_file(self):
-        pass
+        print(self.fullpath)
+        if self.fullpath != "":
+            with open(self.fullpath, 'w') as file:
+                file.write(str(self.Markdown.toPlainText()))
+                print("Finished saving file.")
+        else:
+            self.save_as_file()
 
     def save_as_file(self):
         filedialog = QFileDialog()
-        filename = QFileDialog.getSaveFileName(filedialog, 'Save As...', os.path.expanduser('~'),
-                                               "Markdown (*.md, *.markdown); Text (*.txt)")
-        if filename:
-            print(filename)
-            with open(filename, 'w') as file:
+        self.decide_path()
+        self.fullpath = QFileDialog.getSaveFileName(filedialog, 'Save As...', self.filepath,
+                                                    "Markdown (*.md, *.markdown); Text (*.txt)")
+        if self.fullpath:
+            with open(self.fullpath, 'w') as file:
+                self.learn_path(self.fullpath)
                 file.write(str(self.Markdown.toPlainText()))
         else:
-            print("'Save As' operation closed.")
+            print("'Save As' operation stopped.")
 
     def exit_program(self):
         # make modal via dialog box--if work is unsaved. But for now, just close. // diff
