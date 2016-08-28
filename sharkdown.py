8f8f54c687e0b5c1d56ec4225884915109fc734c
ronrihoo <ron.rihoo@gmail.com>
2016-08-27 22:27:44 -0500

    Update

--- sharkdown.py
+++ sharkdown.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from PyQt4.QtGui import *
 
@@ -25,16 +26,16 @@ class MainWindow(QMainWindow, sharkdown_ui.Ui_Sharkdown):
         self.actionEditor_Viewer.setText(AppConstants.data['actions']['editor_viewer'])
         self.actionEditor_Only.setText(AppConstants.data['actions']['editor_only'])
         self.actionViewer_Only.setText(AppConstants.data['actions']['viewer_only'])
-        self.actionEditor_Viewer.triggered.connect(self.editor_viewer_view)
-        self.actionEditor_Only.triggered.connect(self.editor_only_view)
-        self.actionViewer_Only.triggered.connect(self.viewer_only_view)
+        self.actionEditor_Viewer.triggered.connect(self.split_view)
+        self.actionEditor_Only.triggered.connect(self.editor_only)
+        self.actionViewer_Only.triggered.connect(self.viewer_only)
         # MENU - HELP
         self.actionDocs.setText(AppConstants.data['actions']['docs'])
         self.actionAbout.setText(AppConstants.data['actions']['about'])
         self.actionDocs.triggered.connect(self.send_to_docs)
         self.actionAbout.triggered.connect(self.about_popup)
         # MAIN FUNCTIONALITY
-        self.Markdown.setText("This or the Apocalypse")             # initial text for testing
+        self.Markdown.setText("This or the Apocalypse")  # initial text for testing
         self.Markdown.textChanged.connect(self.parse_and_convert)
         self.show()
         # CYCLE-SUPPORT (ERROR-CHECKING)
@@ -44,20 +45,21 @@ class MainWindow(QMainWindow, sharkdown_ui.Ui_Sharkdown):
         plain = self.Markdown.toPlainText()
         # ... parse... convert... return
         self.HtmlViewer.setHtml(plain)
-        print(plain)            # to see unformatted text (for testing)
+        print(plain)  # to see unformatted text (for testing)
 
     def new_file(self):
         self.Markdown.setText('')
 
     def load_file(self):
-        widget = QFileDialog()
+        filedialog = QFileDialog()
         try:
-            filename = QFileDialog.getOpenFileName(widget, 'Open File', '/')
+            filename = QFileDialog.getOpenFileName(filedialog, 'Open File', os.path.expanduser('~'),
+                                                   "Markdown (*.md, *.markdown); Text (*.txt)")
             self.Markdown.setText('')
             with open(filename, 'r') as f:
                 self.Markdown.insertPlainText(f.read())
         except:
-            # we'll do something later
+            print("'Load' operation closed.")
             pass
 
     def save_file(self):
@@ -65,27 +67,28 @@ class MainWindow(QMainWindow, sharkdown_ui.Ui_Sharkdown):
 
     def save_as_file(self):
         filedialog = QFileDialog()
-        filename = QFileDialog.getSaveFileName(filedialog, 'Dialog Title', '/')
+        filename = QFileDialog.getSaveFileName(filedialog, 'Save As...', os.path.expanduser('~'),
+                                               "Markdown (*.md, *.markdown); Text (*.txt)")
         if filename:
             print(filename)
             with open(filename, 'w') as file:
                 file.write(str(self.Markdown.toPlainText()))
         else:
-            print("No filename given.")
+            print("'Save As' operation closed.")
 
     def exit_program(self):
-        # make modal via dialog box--if work is unsaved. But for now, just close.
+        # make modal via dialog box--if work is unsaved. But for now, just close. // diff
         self.close()
 
-    def editor_viewer_view(self):
+    def split_view(self):
         self.Markdown.setVisible(True)
         self.HtmlViewer.setVisible(True)
 
-    def editor_only_view(self):
+    def editor_only(self):
         self.Markdown.setVisible(True)
         self.HtmlViewer.setVisible(False)
 
-    def viewer_only_view(self):
+    def viewer_only(self):
         self.Markdown.setVisible(False)
         self.HtmlViewer.setVisible(True)
 
@@ -94,6 +97,7 @@ class MainWindow(QMainWindow, sharkdown_ui.Ui_Sharkdown):
 
     def about_popup(self):
         pass
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
